4e1821ba8f8bdf952e227a2c1f1f97dd9156ffd8
Athena Hernandez <athena.hernandez.23@bishops.com>
2022-09-28 23:53:04 -0700

    Created function to create closed contour around paper

--- project/main.py
+++ project/main.py
@@ -1,61 +1,78 @@
+"""
+    Name: Athena Hernandez
+    Date: September 29, 2022
+    Description: This programs scans a paper :)
+"""
+
+# from pyimagesearch.transform import four_point_transform
+# from skimage.filters import threshold_local
 import numpy as np
-import cv2
 import argparse
+import cv2 as cv
+import imutils
 
+def findCnt(edged):
+    cnts = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:5]
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        perimeter = cv.arcLength(c, True)
+        approx = cv.approxPolyDP(c, 0.02 * perimeter, True)
+        # if our approximated contour has four points, then we
+        # can assume that we have found our screen
+        if len(approx) == 4:
+            return approx
 
+def main():
+    # parse = argparse.ArgumentParser()
+    # parse.add_argument('-i', '--image', required=True, help='path to image')
+    # args = vars(parse.parse_args())
 
-# parse = argparse.ArgumentParser()
-# parse.add_argument('-i', '--image', required=True, help='path to image')
-# args = vars(parse.parse_args())
+    # Image path given directly instead of console argument
+    path = "images/image.jpg"
+    img = cv.imread(path) 
 
-path = "images/image.jpg"
-img = cv2.imread(path)
+    # Resize image so functions well with OpenCV
+    # img = cv.resize(img, (1300, 800))
+    original_img = img.copy()
 
-# Resize image so functions well with OpenCV
-img = cv2.resize(img, (1300, 800))
-original_img = img.copy()
+    # Grayscale image
+    grayscaled = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow("Grayscaled", grayscaled)
+    cv.waitKey(0)
 
-# STEP 1: Grayscale image
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Grayscale", gray_img)
-cv2.waitKey(0)
+    # Blur image
+    blurred = cv.GaussianBlur(grayscaled, (5, 5), 0)
+    cv.imshow("Blurred", blurred)
+    cv.waitKey(0)
 
-# STEP 2: Blur image
-blur_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-cv2.imshow("Blur", blur_img)
-cv2.waitKey(0)
+    # Edge image
+    edged = cv.Canny(blurred, 30, 50)
+    cv.imshow("Edged", edged)
+    cv.waitKey(0)
 
-# STEP 3: Edge detection
-edge_img = cv2.Canny(blur_img, 30, 50)
-cv2.imshow("Canny Edges", edge_img)
-cv2.waitKey(0)
+    # Find paper's contour
+    cnt = findCnt(edged)
+    # show the contour (outline) of the piece of paper
+    cv.drawContours(img, [cnt], -1, (0, 255, 0), 2)
+    cv.imshow("Outline", img)
+    cv.waitKey(0)
 
-# cv2.destroyAllWindows()
+    # Warp image
 
-# STEP 4: Find countours
-img, contours = cv2.findContours(edge_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-big = max(contours, key = cv2.contourArea)
-# Find largest countour which is the page outline; search reverse order
-# contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    cv.destroyAllWindows()
+
+    # warped = four_point_transform(img, screenCnt.reshape(4, 2))
+    # convert the warped image to grayscale
+    warped = cv.cvtColor(warped, cv.COLOR_BGR2GRAY)
+    cv.imshow("Scanned", cv.resize(warped, (600, 800)))
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
 
 # Affine
 # Live video feed; text isolation; see if text is vertical 
-
-# for contour in contours:
-#     cv2.arcLength(contour, True) # Tries to find closed shape
-#     approx = cv2.approxPolyDP(contour,)
-
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required = True,
-# help = "Path to the image")
-# args = vars(ap.parse_args())
-
-# img = cv2.imread("test.png")
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# gray = cv2.subtract(255,gray)
-# ret,thresh = cv2.threshold(gray,5,255,cv2.THRESH_TOZERO)
-# kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
-# kernel2 = np.ones((3,3),np.uint8)
-# erosion = cv2.erode(thresh,kernel2,iterations = 1)
-# dilation = cv2.dilate(erosion,kernel1,iterations = 7)
-
