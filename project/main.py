1b25bbdeafeab6baf4f5259e2d37c1576aa5ce76
Athena Hernandez <athena.hernandez.23@bishops.com>
2022-10-03 09:55:37 -0700

    Cleaned up comments

--- project/main.py
+++ project/main.py
@@ -14,73 +14,59 @@ import imutils
 from imutils.perspective import four_point_transform as fourPointTransform
 import pytesseract
 from pytesseract import Output
-from colorama import Fore, Back, Style
+from colorama import Fore
 
 def orderCorners(corners):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "float32")
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = corners.sum(axis = 1)
-	rect[0] = corners[np.argmin(s)]
-	rect[2] = corners[np.argmax(s)]
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(corners, axis = 1)
-	rect[1] = corners[np.argmin(diff)]
-	rect[3] = corners[np.argmax(diff)]
+    """ Obtains a consistent order of points and unpacks them individually"""
+    # List of coordinates starting at top-left going clockwise
+    rect = np.zeros((4, 2), dtype = "float32")        
+
+    s = corners.sum(axis = 1)                                           # Top-left = smallest sum; bottom-right = largest sum
+    rect[0] = corners[np.argmin(s)]
+    rect[2] = corners[np.argmax(s)]
+    
+    # Computer other points
+    diff = np.diff(corners, axis = 1)
+    rect[1] = corners[np.argmin(diff)]
+    rect[3] = corners[np.argmax(diff)]
 	# return the ordered coordinates
-	return rect
+    return rect                                                         # Ordered coordinates
 
 def warp(img, corners):
-	# obtain a consistent order of the points and unpack them
-	# individually
-	rect = orderCorners(corners)
-	(tl, tr, br, bl) = rect
-	# compute the width of the new image, which will be the
-	# maximum distance between bottom-right and bottom-left
-	# x-coordiates or the top-right and top-left x-coordinates
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
-	# compute the height of the new image, which will be the
-	# maximum distance between the top-right and bottom-right
-	# y-coordinates or the top-left and bottom-left y-coordinates
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
-	# now that we have the dimensions of the new image, construct
-	# the set of destination points to obtain a "birds eye view",
-	# (i.e. top-down view) of the image, again specifying points
-	# in the top-left, top-right, bottom-right, and bottom-left
-	# order
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
-	# compute the perspective transform matrix and then apply it
-	M = cv.getPerspectiveTransform(rect, dst)
-	warped = cv.warpPerspective(img, M, (maxWidth, maxHeight))
-	# return the warped image
-	return warped
+    """ Aligns image to aerial view"""
+    rect = orderCorners(corners)
+    (tl, tr, br, bl) = rect
+     
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))       # Distance between bottom right and left
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))       # Distance between top right and left
+    maxWidth = max(int(widthA), int(widthB))                                # Finds largest edge to use as base to warp
+    
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))      # Same idea for height
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # Create points for warped image
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+
+    # Compute perspective transform matrix then apply it
+    M = cv.getPerspectiveTransform(rect, dst)
+    warped = cv.warpPerspective(img, M, (maxWidth, maxHeight))
+    return warped                                                           # Warped image
 
 def largestCnt(edged):
+    """ Finds the largest rectangular contour """
     cnts = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:5]
-    # loop over the contours
-    for c in cnts:
-        # approximate the contour
-        perimeter = cv.arcLength(c, True)
-        approx = cv.approxPolyDP(c, 0.02 * perimeter, True)
-        # if our approximated contour has four points, then we
-        # can assume that we have found our screen
-        if len(approx) == 4:
+    cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:5]   # Largest to smallest
+    
+    for c in cnts:                                                  # Loop over contours
+        perimeter = cv.arcLength(c, True)                   
+        approx = cv.approxPolyDP(c, 0.02 * perimeter, True)         # Approximates shape
+        if len(approx) == 4:                                        # Paper has 4 points
             return approx
 
 def qrScanner(filename):
